0809301f4afb10b47849343804e8576d43a6ffb5
Daniel Thalmann <daniel@thalmann.li>
2022-11-19 20:47:02 +0100

    update playermoove

--- Assets/Scripts/Player/playermoove.cs
+++ Assets/Scripts/Player/playermoove.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class playermoove : MonoBehaviour
 {
     public float speed = 50;
-    private Rigidbody body;
     public float positionY;
     public float positionX;
     public float positionZ;
@@ -13,6 +12,11 @@ public class playermoove : MonoBehaviour
     public Vector3 hauter;
     public Animator anim;
 
+
+
+    private Rigidbody body;
+    private float shoot_timer = 0.5f;
+    private float t = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,8 +44,10 @@ public class playermoove : MonoBehaviour
         body.velocity = velocity * speed;
         //body.velocity = transform.TransformDirection(body.velocity);
         // body.velocity *= speed;
-        if (Input.GetKeyDown(KeyCode.Space))
+        t += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && t > shoot_timer)
         {
+            t = 0;
             Dig();
         }
         hauter = new Vector3(positionX, 0.51f, positionZ);
