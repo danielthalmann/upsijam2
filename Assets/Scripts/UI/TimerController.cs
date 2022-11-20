77b22e4d24506077594998e9cdfa549ac1147258
Jérémie Schreyer <jeremieschreyer@gmail.com>
2022-11-20 12:01:22 +0100

    build 0.0.4

--- Assets/Scripts/UI/TimerController.cs
+++ Assets/Scripts/UI/TimerController.cs
@@ -7,17 +7,22 @@ public class TimerController : MonoBehaviour
 {
     public TextMeshProUGUI counterText;
     public int seconds, minutes;
+    private float initTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
+        seconds = 0;
+        minutes = 0;
+        initTimer = Time.time;
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        minutes = (int)(Time.time / 60f);
-        seconds = (int)(Time.time % 60f);
+        minutes = (int)((Time.time - initTimer) / 60f);
+        seconds = (int)((Time.time - initTimer) % 60f);
         counterText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
     }
 
